8b39d74d5dc94df4ba0a099a360aee066a3b8779
Alexandr <boltenkovs@inbox.ru>
2023-07-02 17:02:55 +0400

    Добавление карты, футера, минификация

---
+++ source/js/main.js
@@ -0,0 +1,9 @@
+import initMenu from './menu.js';
+import initLeaflet from './map.js';
+import initRangeBar from './range-bar.js';
+import initSlider from './slider.js';
+
+initMenu();
+initLeaflet();
+initRangeBar();
+initSlider();
